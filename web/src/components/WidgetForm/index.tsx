c61066afa7b55827275ad6f09db92e9b4885686d
Daniel Hessel <daniel@sharbe.com.br>
2022-05-03 16:09:34 -0300

    feat: creating and separating components from the widget form

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { CloseButton } from "../CloseButton";
+
+import bugImageUrl from "../../assets/bug.svg";
+import ideaImageUrl from "../../assets/idea.svg";
+import thoughtImageUrl from "../../assets/thought.svg";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+
+export const feedbackTypes = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto representando um bug"
+    }
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada representando uma ideia"
+    }
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem de pensamento representando outras coisas quaisquer"
+    }
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+  function handleRestartFeedback() {
+    setFeedbackType(null);
+  }
+
+  return (
+    <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+      ) : (
+        <FeedbackContentStep
+          feedbackType={feedbackType}
+          onFeedbackRestartRequested={handleRestartFeedback}
+        />
+      )}
+
+      <footer className="text-xs text-neural-400">
+        Feito com 🖤 por <a className="underline underline-offset-2" target="_blank" href="https://github.com/danielhessell">Daniel Hessel</a>
+      </footer>
+    </div>
+  )
+}
